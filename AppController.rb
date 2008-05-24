cde87d8b29db7a6a564c2b2ec7d2e4368b10b861
jp_tix <kreatix@gmail.com>
2008-05-24 19:59:45 +0200

    Initial commit

---
+++ AppController.rb
@@ -0,0 +1,136 @@
+require 'osx/cocoa'
+include OSX
+require "tree_constructor"
+
+class RCBBrowserCell < NSBrowserCell
+  attr_accessor :node
+end
+
+
+class RCBAppController < NSObject
+	ib_outlets :browser, :table_view, :text_view, :search_field
+
+  def initialize
+    @selected_cell = nil
+    @methods = []
+    super.init
+    @ri = RCBFastRi.new
+  end
+
+	def awakeFromNib
+	  @browser.setCellClass(RCBBrowserCell)
+	  @browser.setDelegate(self)
+	  @browser.setMaxVisibleColumns(4)
+	  
+	  @table_view.setDataSource(self)
+	  @table_view.setDelegate(self)
+
+    @text_view.setFont(NSFont.fontWithName_size('Monaco', 10.0))
+	  
+	  @tree_constructor = RCBTreeConstructor.new
+    @classes = @tree_constructor.create
+
+    Thread.new do
+      loop { update_method_table; sleep 0.4 }
+    end
+	end
+	
+  def open(sender)
+    panel = NSOpenPanel.openPanel
+    if panel.runModalForTypes(%w(rb)) == NSOKButton
+      begin
+        panel.filenames.each { |f| require(f) }
+      rescue Exception => e
+        NSAlert.alertWithMessageText_defaultButton_alternateButton_otherButton_informativeTextWithFormat(e.message, nil, nil, nil, nil)
+      end
+    end
+    @classes = @tree_constructor.create
+  end
+
+	def search(sender)
+	 Thread.new do
+  	 node = @classes.values.find do |node|
+  	   node.name.split("::").any? { |e| e.downcase == sender.stringValue.downcase }
+  	 end
+  	 return unless node
+	 
+  	 path = [node]
+  	 until (parent = node.superclass) == nil
+  	   node = @classes[parent.name]
+  	   path.unshift(node)
+     end
+   
+     path.each_with_index do |e, idx|
+       if idx == 0
+         @browser.selectRow_inColumn(0, 0)
+  	   else
+  	     @browser.selectRow_inColumn(@classes[e.superclass.name].subclasses.index(e), idx)
+       end
+  	 end
+   end
+	end
+
+	# ==============================
+	# = NSBrowser delegate methods =
+	# ==============================
+  def browser_willDisplayCell_atRow_column(browser, cell, row, column)
+    if column == 0
+      current_node = @classes['Object']
+    else
+      parent_node = parent_node_for_column(column)
+      current_node = parent_node.subclasses[row]
+    end
+    
+    cell.setLeaf(true) if current_node.subclasses.empty?
+	  cell.setStringValue(current_node.name)
+	  cell.node = current_node
+  end
+  
+  def browser_numberOfRowsInColumn(browser, column)
+    if column == 0
+      1
+    else
+      parent_node_for_column(column).subclasses.size
+    end
+	end
+	
+	# ===================================
+	# = NSTableView data source methods =
+	# ===================================
+  def tableView_objectValueForTableColumn_row(table_view, column, row)
+    @methods[row]
+  end
+  
+  def numberOfRowsInTableView(table_view)
+    @methods.size
+  end
+  
+  # ================================
+  # = NSTableView delegate methods =
+  # ================================
+  def tableView_shouldSelectRow(table_view, row)
+    Thread.new do
+      full_name = "#{@selected_class}##{@methods[row]}"
+      result = @ri.info(full_name)
+      @text_view.setString(result ? result.to_s : "Nothing known about #{full_name}")
+    end
+    true
+  end
+  
+	private
+
+	def parent_node_for_column(column)
+    unless column == 0
+  	  @browser.selectedCellInColumn(column - 1).node
+	  end
+	end
+
+  def update_method_table
+    if cell = @browser.selectedCell
+      @selected_class = cell.node.name
+      @methods = cell.node.instance_methods
+      @table_view.reloadData
+    end
+  end
+end
+
---
+++ tree_constructor.rb
@@ -0,0 +1,71 @@
+class RCBTreeConstructor
+  attr_reader :classes
+  
+  def initialize
+    @classes = {}
+    require_stdlib
+  end
+  
+  def create
+    ObjectSpace.each_object(Class) do |obj|
+      next if obj.name =~ /^RCB/
+      @classes[obj.name] ||= RCBClassNode.new(obj) 
+    end
+    @classes.delete(nil)
+    @classes.values.each do |node|
+      if parent_node = @classes[node.superclass.to_s]
+        parent_node.subclasses << node
+      end
+    end
+    
+    @classes.values.each { |node| node.subclasses.sort!  }
+    @classes
+  end
+  
+  private
+
+  def require_stdlib
+    Dir["/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/lib/ruby/1.8/*.rb"].each do |f|
+      next if f =~ /debug|profile|cgi-lib|mathn|tk|tempfile/
+      # log("*** -> " + f)
+      require(f)
+    end
+  end
+  
+end
+
+class RCBClassNode
+  attr_accessor :name, :superclass, :subclasses
+  
+  def initialize(klass)
+    @class = klass
+    @name = klass.name
+    @superclass = klass.superclass
+    @subclasses = []
+  end
+  
+  def <=>(other)
+    @name <=> other.name
+  end
+  
+  def instance_methods
+    if @name == 'Object'
+      Object.instance_methods.sort
+    else
+      (@class.instance_methods - @class.ancestors.map { |e| e.instance_methods } - Object.methods).sort
+    end
+  end
+  
+  def class_methods
+    (@class.methods - @class.ancestors.map { |e| e.methods }  - Object.methods).sort
+  end
+end
+
+
+
+if __FILE__ == $0
+  require "pp"
+  t = RCBTreeConstructor.new
+  t.create
+  pp t.classes
+end
